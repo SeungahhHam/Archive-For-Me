802dd81fc3f754a35c4ea5807445aff09ae74d5f
SeungahhHam <102564850+SeungahhHam@users.noreply.github.com>
2026-01-12 16:45:43 +0900

    Update getRecordMap.ts

--- src/apis/notion-client/getRecordMap.ts
+++ src/apis/notion-client/getRecordMap.ts
@@ -3,6 +3,5 @@ import { NotionAPI } from "notion-client"
 export const getRecordMap = async (pageId: string) => {
   const api = new NotionAPI()
   const recordMap = await api.getPage(pageId)
-  console.log(pageId)
   return recordMap
 }
