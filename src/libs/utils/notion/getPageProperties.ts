b8974adcb7e4b9f9937a39e6b80df998a9eb1c89
SeungahhHam <102564850+SeungahhHam@users.noreply.github.com>
2026-01-13 11:06:23 +0900

    Update getPageProperties.ts

--- src/libs/utils/notion/getPageProperties.ts
+++ src/libs/utils/notion/getPageProperties.ts
@@ -10,7 +10,7 @@ async function getPageProperties(
 ) {
   const api = new NotionAPI()
   const rawProperties = Object.entries(block?.[id]?.value?.properties || [])
-  console.log("[DEBUG][rawProperties]", rawProperties)
+  console.log("[Notion][rawProperties]", JSON.stringify(rawProperties, null, 2))
   const customTypes = ["date", "select", "multi_select", "person", "file", "formula", "relation", "checkbox", "rollup"]
   const properties: any = { id }
 
